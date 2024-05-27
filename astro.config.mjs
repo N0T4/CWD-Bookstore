79df3e27bb19895911594fd00ac394ad023b3d3f
N0T4 <118122479+N0T4@users.noreply.github.com>
2024-05-27 16:33:37 +0100

    contact us is done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,21 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   build: {
+    format: 'directory',
     rollupOptions: {
-      external: ['svelte']
-    }
+      external: ['svelte'],
+    },
   },
-  base: '/',
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[ext]',
+          chunkFileNames: 'assets/[name].js',
+          entryFileNames: 'assets/[name].js',
+        },
+      },
+    },
+  },
+  base: './', 
 });
